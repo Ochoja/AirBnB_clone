27089d87e1d73544ba8233acd046332a8a4a5b24
Daniel Ochoja <danielochoja@yahoo.com>
2023-02-12 21:39:51 +0000

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,6 @@ class BaseModel:
     """Defines all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
         """Constructor"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            storage.new(self)
-        else:
             if "id" not in kwargs.keys():
                 self.id = str(uuid.uuid4())
 
@@ -35,6 +29,9 @@ class BaseModel:
                         setattr(self, key, date)
                     else:
                         setattr(self, key, value)
+
+            if len(kwargs) == 0:
+                storage.new(self)
 
     def save(self):
         """updates the public instance attribute updated_at
